0289789b7ffcc75ef972c88ce54d24916d05fa1a
Xavier Redondo <xavier.redondo@axa-groupsolutions.com>
2017-08-03 09:52:06 +0200

    explanations of each case (newbie level)

--- src/main-promises.js
+++ src/main-promises.js
@@ -15,6 +15,7 @@ const createAMushroom = () => {
 
 const eatFood = food => {
   return new Promise((resolve, reject) => {
+    console.log('Estoy en eat food');
     setTimeout(() => {
       console.log( `The ${food} has been eaten!`);
       resolve();
@@ -52,13 +53,16 @@ const example1 = () => {
 // 
 // Two details:
 // Detail 1: finalHandler receives undefined because there is no return in eatFood and then the promise returns undefined
-// Detail 2: finalHandler is called as soon as createMushroom finishes 
+// Detail 2: finalHandler is called as soon as createMushroom finishes becase without return the
+// second then(finalHandler) starts to execute as soon as createMushroom finishes. This happens
+// because then acts as an observer that only waits if there is a promise being returned, but does
+// not wait if nothing is being returned.
 const example2 = () => {
   createAMushroom()
     .then(function(food) {
       eatFood(food);
     })
-    .then(finalHandler);
+    .then(finalHandler)
 };
 
 // createAMushroom
@@ -67,7 +71,10 @@ const example2 = () => {
 // |------------/2s/----|          |
 //                                 finalHandler(Mushroom)
 //                                 |--------------------|
-//
+// 
+// Since eatFood() is directly called instead of being inside the then(resolve) callback, the eatFood
+// is executed asyncrhonously as soon as createMushroom is put in the eventLoop (that is the reason that
+// we have undefined as eaten). 
 const example3 = () => {
   createAMushroom()
     .then(eatFood())
@@ -80,10 +87,13 @@ const example3 = () => {
 //                             |------/2s/------|
 //                                              finalHandler(result of eatFood is undefined)
 //                                              |-------------|
+//
+// This is like the example1 but better and more concisely written. 
+// In here eatFood and finalHandler are functions that return a promise.
 const example4 = () => {
   createAMushroom()
     .then(eatFood)
     .then(finalHandler);
 }
 
-example4();
+example3();
